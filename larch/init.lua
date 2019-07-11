44fe33408c1410c500152863281a6d57a9b14d8a
runs <runs@runs-PC.runs.es>
2019-07-11 23:11:36 +0200

    larch added

--- larch/init.lua
+++ larch/init.lua
@@ -1,24 +1,23 @@
 --
--- Ebony
+-- Larch
 --
-
-local modname = "ebony"
+local modname = "larch"
 local modpath = minetest.get_modpath(modname)
 local mg_name = minetest.get_mapgen_setting("mg_name")
 
 -- internationalization boilerplate
 local S = minetest.get_translator(minetest.get_current_modname())
 
--- Ebony
+-- Larch
 
-local function grow_new_ebony_tree(pos)
+local function grow_new_larch_tree(pos)
 	if not default.can_grow(pos) then
 		-- try a bit later again
 		minetest.get_node_timer(pos):start(math.random(240, 600))
 		return
 	end
 
-	minetest.place_schematic({x = pos.x-5, y = pos.y, z = pos.z-5}, modpath.."/schematics/ebony.mts", "0", nil, true)
+	minetest.place_schematic({x = pos.x-5, y = pos.y, z = pos.z-5}, modpath.."/schematics/larch.mts", "0", nil, true)
 end
 
 --
@@ -28,21 +27,21 @@ end
 if mg_name ~= "v6" and mg_name ~= "singlenode" then
 	minetest.register_decoration({
 		deco_type = "schematic",
-		place_on = {"default:dirt_with_rainforest_litter"},
+		place_on = {"default:dirt_with_coniferous_litter"},
 		sidelen = 16,
 		noise_params = {
-			offset = 0.005,
-			scale = 0.002,
+			offset = 0.0005,
+			scale = 0.0005,
 			spread = {x = 250, y = 250, z = 250},
 			seed = 2,
 			octaves = 3,
 			persist = 0.66
 		},
-		biomes = {"rainforest"},
-		height = 2,
+		biomes = {"coniferous_forest"},
 		y_min = 1,
-		y_max = 62,
-		schematic = modpath.."/schematics/ebony.mts",
+		y_max = 32,
+		place_offset_y = 1,
+		schematic = modpath.."/schematics/larch.mts",
 		flags = "place_center_x, place_center_z, force_placement",
 		rotation = "random",
 	})
@@ -52,17 +51,17 @@ end
 -- Nodes
 --
 
-minetest.register_node("ebony:sapling", {
-	description = S("Ebony Tree Sapling"),
+minetest.register_node("larch:sapling", {
+	description = S("Larch Tree Sapling"),
 	drawtype = "plantlike",
 	visual_scale = 1.0,
-	tiles = {"ebony_sapling.png"},
-	inventory_image = "ebony_sapling.png",
-	wield_image = "ebony_sapling.png",
+	tiles = {"larch_sapling.png"},
+	inventory_image = "larch_sapling.png",
+	wield_image = "larch_sapling.png",
 	paramtype = "light",
 	sunlight_propagates = true,
 	walkable = false,
-	on_timer = grow_new_ebony_tree,
+	on_timer = grow_new_larch_tree,
 	selection_box = {
 		type = "fixed",
 		fixed = {-4 / 16, -0.5, -4 / 16, 4 / 16, 7 / 16, 4 / 16}
@@ -77,7 +76,7 @@ minetest.register_node("ebony:sapling", {
 
 	on_place = function(itemstack, placer, pointed_thing)
 		itemstack = default.sapling_on_place(itemstack, placer, pointed_thing,
-			"ebony:sapling",
+			"larch:sapling",
 			-- minp, maxp to be checked, relative to sapling pos
 			-- minp_relative.y = 1 because sapling pos has been checked
 			{x = -2, y = 1, z = -2},
@@ -89,12 +88,12 @@ minetest.register_node("ebony:sapling", {
 	end,
 })
 
-minetest.register_node("ebony:trunk", {
-	description = S("Ebony Trunk"),
+minetest.register_node("larch:trunk", {
+	description = S("Larch Trunk"),
 	tiles = {
-		"ebony_trunk_top.png",
-		"ebony_trunk_top.png",
-		"ebony_trunk.png"
+		"larch_trunk_top.png",
+		"larch_trunk_top.png",
+		"larch_trunk.png"
 	},
 	groups = {tree = 1, choppy = 2, oddly_breakable_by_hand = 1, flammable = 2},
 	sounds = default.node_sound_wood_defaults(),
@@ -106,10 +105,10 @@ minetest.register_node("ebony:trunk", {
 	end
 })
 
--- ebony wood
-minetest.register_node("ebony:wood", {
-	description = S("Ebony Wood"),
-	tiles = {"ebony_wood.png"},
+-- larch wood
+minetest.register_node("larch:wood", {
+	description = S("Larch Wood"),
+	tiles = {"larch_wood.png"},
 	paramtype2 = "facedir",
 	place_param2 = 0,
 	is_ground_content = false,
@@ -117,14 +116,14 @@ minetest.register_node("ebony:wood", {
 	sounds = default.node_sound_wood_defaults(),
 })
 
--- ebony tree leaves
-minetest.register_node("ebony:leaves", {
-	description = S("Ebony Leaves"),
+-- larch tree leaves
+minetest.register_node("larch:leaves", {
+	description = S("Larch Leaves"),
 	drawtype = "allfaces_optional",
 	visual_scale = 1.2,
-	tiles = {"ebony_leaves.png"},
-	inventory_image = "ebony_leaves.png",
-	wield_image = "ebony_leaves.png",
+	tiles = {"larch_leaves.png"},
+	inventory_image = "larch_leaves.png",
+	wield_image = "larch_leaves.png",
 	paramtype = "light",
 	walkable = true,
 	waving = 1,
@@ -132,71 +131,30 @@ minetest.register_node("ebony:leaves", {
 	drop = {
 		max_items = 1,
 		items = {
-			{items = {"ebony:sapling"}, rarity = 20},
-			{items = {"ebony:leaves"}}
+			{items = {"larch:sapling"}, rarity = 20},
+			{items = {"larch:leaves"}}
 		}
 	},
 	sounds = default.node_sound_leaves_defaults(),
 	after_place_node = default.after_place_leaves,
 })
 
---
--- Creeper/Vines...
---
-
-minetest.register_node("ebony:creeper", {
-	description = S("Ebony Creeper"),
+minetest.register_node("larch:moss", {
+	description = S("Larch Moss"),
 	drawtype = "nodebox",
 	walkable = true,
 	paramtype = "light",
 	paramtype2 = "facedir",
-	tiles = {"ebony_creeper.png"},
-	inventory_image = "ebony_creeper.png",
-	wield_image = "ebony_creeper.png",
+	tiles = {"larch_moss.png"},
+	inventory_image = "larch_moss.png",
+	wield_image = "larch_moss.png",
 	node_box = {
 		type = "fixed",
 		fixed = {-0.5, -0.5, 0.49, 0.5, 0.5, 0.5}
-	},
-	groups = {
-		snappy = 2, flammable = 3, oddly_breakable_by_hand = 3, choppy = 2, carpet = 1, leafdecay = 3, leaves = 1
-	},
-	sounds = default.node_sound_leaves_defaults(),
-})
-
-minetest.register_node("ebony:creeper_leaves", {
-	description = S("Ebony Creeper with Leaves"),
-	drawtype = "nodebox",
-	walkable = true,
-	paramtype = "light",
-	paramtype2 = "facedir",
-	tiles = {"ebony_creeper_leaves.png"},
-	inventory_image = "ebony_creeper_leaves.png",
-	wield_image = "ebony_creeper_leaves.png",
-	node_box = {
-		type = "fixed",
-		fixed = {-0.5, -0.5, 0.49, 0.5, 0.5, 0.5}
-	},
-	groups = {
-		snappy = 2, flammable = 3, oddly_breakable_by_hand = 3, choppy = 2, carpet = 1, leafdecay = 3, leaves = 1
-	},
-	sounds = default.node_sound_leaves_defaults(),
-})
-
-minetest.register_node("ebony:liana", {
-	description = S("Ebony Liana"),
-	drawtype = "nodebox",
-	walkable = true,
-	paramtype = "light",
-	paramtype2 = "facedir",
-	tiles = {"ebony_liana.png"},
-	inventory_image = "ebony_liana.png",
-	wield_image = "ebony_liana.png",
-	node_box = {
-		type = "fixed",
-		fixed = {-0.5, -0.5, 0.0, 0.5, 0.5, 0.0}
 	},
 	groups = {
 		snappy = 2, flammable = 3, oddly_breakable_by_hand = 3, choppy = 2, carpet = 1, leafdecay = 3, leaves = 1,
+		falling_node = 1
 	},
 	sounds = default.node_sound_leaves_defaults(),
 })
@@ -210,34 +168,35 @@ minetest.register_node("ebony:liana", {
 --
 
 minetest.register_craft({
-	output = "ebony:wood 4",
-	recipe = {{"ebony:trunk"}}
+	output = "larch:wood 4",
+	recipe = {{"larch:trunk"}}
 })
+
 
 minetest.register_craft({
 	type = "fuel",
-	recipe = "ebony:trunk",
+	recipe = "larch:trunk",
 	burntime = 30,
 })
 
 minetest.register_craft({
 	type = "fuel",
-	recipe = "ebony:wood",
+	recipe = "larch:wood",
 	burntime = 7,
 })
 
 
 minetest.register_lbm({
-	name = "ebony:convert_ebony_saplings_to_node_timer",
-	nodenames = {"ebony:sapling"},
+	name = "larch:convert_larch_saplings_to_node_timer",
+	nodenames = {"larch:sapling"},
 	action = function(pos)
 		minetest.get_node_timer(pos):start(math.random(1200, 2400))
 	end
 })
 
 default.register_leafdecay({
-	trunks = {"ebony:trunk"},
-	leaves = {"ebony:leaves"},
+	trunks = {"larch:trunk"},
+	leaves = {"larch:leaves"},
 	radius = 3,
 })
 
@@ -245,12 +204,12 @@ default.register_leafdecay({
 
 if minetest.get_modpath("stairs") ~= nil then	
 	stairs.register_stair_and_slab(
-		"ebony_trunk",
-		"ebony:trunk",
+		"larch_trunk",
+		"larch:trunk",
 		{choppy = 2, oddly_breakable_by_hand = 2, flammable = 2},
-		{"ebony_wood.png"},
-		S("Ebony Stair"),
-		S("Ebony Slab"),
+		{"larch_wood.png"},
+		S("Larch Tree Stair"),
+		S("Larch Tree Slab"),
 		default.node_sound_wood_defaults()
 	)
 end
