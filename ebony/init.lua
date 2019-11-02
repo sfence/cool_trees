932e9adfd9311ab69302ba007f5888d956d00457
runs <runs@runs-PC.runs.es>
2019-11-02 18:33:48 +0100

    persimmon

--- ebony/init.lua
+++ ebony/init.lua
@@ -201,6 +201,31 @@ minetest.register_node("ebony:liana", {
 	sounds = default.node_sound_leaves_defaults(),
 })
 
+--Persimmon Kaki
+
+minetest.register_node("ebony:persimmon", {
+	description = S("Persimmon"),
+	drawtype = "plantlike",
+	tiles = {"ebony_persimmon.png"},
+	inventory_image = "ebony_persimmon.png",
+	paramtype = "light",
+	sunlight_propagates = true,
+	walkable = false,
+	is_ground_content = false,
+	selection_box = {
+		type = "fixed",
+		fixed = {-3 / 16, -7 / 16, -3 / 16, 3 / 16, 4 / 16, 3 / 16}
+	},
+	groups = {fleshy = 3, dig_immediate = 3, flammable = 2,
+		leafdecay = 3, leafdecay_drop = 1},
+	on_use = minetest.item_eat(4),
+	sounds = default.node_sound_leaves_defaults(),
+
+	after_place_node = function(pos, placer, itemstack)
+		minetest.set_node(pos, {name = "ebony:persimmon", param2 = 1})
+	end,
+})
+
 --
 -- Craftitems
 --
